55f55dd8e871536be1332eae57749d9e26f88011
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2022-03-09 15:30:14 +0530

    simple design, registering components & onclicklistener

--- app/src/main/java/com/kriti/android1_30/MainActivity.java
+++ app/src/main/java/com/kriti/android1_30/MainActivity.java
@@ -3,6 +3,10 @@ package com.kriti.android1_30;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Registering the components
+        Button button = findViewById(R.id.button);
+        TextView text = findViewById(R.id.textView);
+
+        // Code for whenever button is clicked
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText("Button is now clicked!");
+            }
+        });
     }
 }
