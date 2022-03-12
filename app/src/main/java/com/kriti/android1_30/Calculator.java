0f728f6209cc6ab974833358a02fbd975801bd2e
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2022-03-12 15:03:58 +0530

    single listener on multiple buttons

--- app/src/main/java/com/kriti/android1_30/Calculator.java
+++ app/src/main/java/com/kriti/android1_30/Calculator.java
@@ -10,39 +10,49 @@ import android.widget.TextView;
 
 public class Calculator extends AppCompatActivity {
 
+    EditText firstEditText, secondEditText;
+    TextView textView;
+    Button addButton, subButton, mulButton, divButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
 
         // Registering
-        EditText firstEditText = findViewById(R.id.firstEditText);
-        EditText secondEditText = findViewById(R.id.secondEditText);
-        TextView textView = findViewById(R.id.textViewResult);
-        Button addButton = findViewById(R.id.buttonAdd);
-        Button subButton = findViewById(R.id.buttonSub);
-        Button mulButton = findViewById(R.id.buttonMul);
-        Button divButton = findViewById(R.id.buttonDiv);
+        firstEditText = findViewById(R.id.firstEditText);
+        secondEditText = findViewById(R.id.secondEditText);
+        textView = findViewById(R.id.textViewResult);
+        addButton = findViewById(R.id.buttonAdd);
+        subButton = findViewById(R.id.buttonSub);
+        mulButton = findViewById(R.id.buttonMul);
+        divButton = findViewById(R.id.buttonDiv);
 
-        // Listeners
-        addButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                double firstNumber = Double.parseDouble(firstEditText.getText().toString());
-                double secondNumber = Double.parseDouble(secondEditText.getText().toString());
-                double result = firstNumber + secondNumber;
-                textView.setText("Result: " + String.valueOf(result));
-            }
-        });
-
-        subButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                double firstNumber = Double.parseDouble(firstEditText.getText().toString());
-                double secondNumber = Double.parseDouble(secondEditText.getText().toString());
-                double result = firstNumber - secondNumber;
-                textView.setText("Result: " + String.valueOf(result));
-            }
-        });
+        addButton.setOnClickListener(buttonListener);
+        subButton.setOnClickListener(buttonListener);
+        mulButton.setOnClickListener(buttonListener);
+        divButton.setOnClickListener(buttonListener);
     }
+
+    // Listener for all the buttons
+    View.OnClickListener buttonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            double firstNumber = Double.parseDouble(firstEditText.getText().toString());
+            double secondNumber = Double.parseDouble(secondEditText.getText().toString());
+            double result = 0;
+            int id = view.getId();
+
+            if (id == R.id.buttonAdd)
+                result = firstNumber + secondNumber;
+            else if (id == R.id.buttonSub)
+                result = firstNumber - secondNumber;
+            else if (id == R.id.buttonMul)
+                result = firstNumber * secondNumber;
+            else if (id == R.id.buttonDiv)
+                result = firstNumber / secondNumber;
+
+            textView.setText("Result: " + String.valueOf(result));
+        }
+    };
 }
