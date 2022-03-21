8111ad59b67a079df34fa44caec5ab300fc855b2
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2022-03-21 14:23:25 +0530

    file handling

---
+++ app/src/main/java/com/kriti/android1_30/FileOutput.java
@@ -0,0 +1,14 @@
+package com.kriti.android1_30;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FileOutput extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file_output);
+    }
+}
