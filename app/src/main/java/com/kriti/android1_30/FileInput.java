8c6c1605b3670b1afd9ce8fb683354568e2af1aa
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2022-03-22 13:43:41 +0530

    file handling

--- app/src/main/java/com/kriti/android1_30/FileInput.java
+++ app/src/main/java/com/kriti/android1_30/FileInput.java
@@ -3,6 +3,7 @@ package com.kriti.android1_30;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,7 +15,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 
-public class FileInput extends AppCompatActivity implements View.OnClickListener{
+public class FileInput extends AppCompatActivity {
 
     EditText editText;
     Button readButton, saveButton;
@@ -29,11 +30,8 @@ public class FileInput extends AppCompatActivity implements View.OnClickListener
         readButton = findViewById(R.id.readFileButton);
         saveButton = findViewById(R.id.saveFileButton);
 
-//        readButton.setOnClickListener(buttonListener);
-//        saveButton.setOnClickListener(buttonListener);
-
-        readButton.setOnClickListener(this);
-        saveButton.setOnClickListener(this);
+        readButton.setOnClickListener(buttonListener);
+        saveButton.setOnClickListener(buttonListener);
     }
 
     View.OnClickListener buttonListener = new View.OnClickListener() {
@@ -45,7 +43,7 @@ public class FileInput extends AppCompatActivity implements View.OnClickListener
             {
                 if (id == R.id.readFileButton)
                 {
-                    File file = new File(FileInput.this.getFilesDir() + "/" + FILENAME);
+                File file = new File(FileInput.this.getFilesDir() + "/" + FILENAME);
                     FileReader fileReader = new FileReader(file);
                     BufferedReader bufferedReader = new BufferedReader(fileReader);
                     String x = "", y;
@@ -57,11 +55,10 @@ public class FileInput extends AppCompatActivity implements View.OnClickListener
                     }
 
                     editText.setText(x);
-
                 }
                 else if (id == R.id.saveFileButton)
                 {
-                    File file = new File(FileInput.this.getFilesDir() + "/" + FILENAME);
+                File file = new File(FileInput.this.getFilesDir() + "/" + FILENAME);
                     FileWriter fileWriter = new FileWriter(file);
                     BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
                     String content = editText.getText().toString();
@@ -77,42 +74,4 @@ public class FileInput extends AppCompatActivity implements View.OnClickListener
         }
     };
 
-    @Override
-    public void onClick(View view) {
-        int id = view.getId();
-
-        try
-        {
-            if (id == R.id.readFileButton)
-            {
-                File file = new File(this.getFilesDir() + "/" + FILENAME);
-                FileReader fileReader = new FileReader(file);
-                BufferedReader bufferedReader = new BufferedReader(fileReader);
-                String x = "", y;
-
-                while ((y = bufferedReader.readLine()) != null)
-                {
-                    x += y;
-                    x += "\n";
-                }
-
-                editText.setText(x);
-
-            }
-            else if (id == R.id.saveFileButton)
-            {
-                File file = new File(this.getFilesDir() + "/" + FILENAME);
-                FileWriter fileWriter = new FileWriter(file);
-                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-                String content = editText.getText().toString();
-                bufferedWriter.write(content);
-                bufferedWriter.flush();
-                Toast.makeText(FileInput.this, "File saved", Toast.LENGTH_LONG).show();
-            }
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-    }
 }
