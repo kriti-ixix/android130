f4a1762e0cd7490d688c5de9fe7cca4bf9c7eb64
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2022-05-11 13:15:51 +0530

    custom recycler view adapter

--- app/src/main/java/com/kriti/android1_30/Movies.java
+++ app/src/main/java/com/kriti/android1_30/Movies.java
@@ -2,13 +2,18 @@ package com.kriti.android1_30;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -18,6 +23,8 @@ public class Movies extends AppCompatActivity {
     RecyclerView recyclerView;
     Button addMovieButton;
     EditText movieEditText;
+    CustomAdapter adapter;
+    String tag = "Recycler View";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +41,20 @@ public class Movies extends AppCompatActivity {
         moviesList.add("Lekh");
         moviesList.add("Qismat");
         moviesList.add("Gangubai");
+
+        adapter = new CustomAdapter();
+        recyclerView.setAdapter(adapter);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        addMovieButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String movie = movieEditText.getText().toString();
+                moviesList.add(movie);
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 
     class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomViewHolder>
@@ -41,8 +62,14 @@ public class Movies extends AppCompatActivity {
         // Registering of components
         class CustomViewHolder extends RecyclerView.ViewHolder
         {
+            ImageView imageView;
+            TextView textView;
+
             public CustomViewHolder(@NonNull View itemView) {
                 super(itemView);
+                Log.d(tag, "View Holder");
+                imageView = itemView.findViewById(R.id.customRecyclerImage);
+                textView = itemView.findViewById(R.id.customRecyclerText);
             }
         }
 
@@ -50,19 +77,25 @@ public class Movies extends AppCompatActivity {
         @NonNull
         @Override
         public CustomAdapter.CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            return null;
+            Log.d(tag, "onCreateViewHolder");
+            LayoutInflater inflater = LayoutInflater.from(Movies.this);
+            View view = inflater.inflate(R.layout.custom_recycler_adapter, parent, false);
+            return new CustomAdapter.CustomViewHolder(view);
         }
 
         // When the view is loaded
         @Override
         public void onBindViewHolder(@NonNull CustomAdapter.CustomViewHolder holder, int position) {
-
+            Log.d(tag, "onBindViewHolder");
+            String movie = moviesList.get(position);
+            holder.textView.setText(movie);
         }
 
         // Number of items to display
         @Override
         public int getItemCount() {
-            return 0;
+            Log.d(tag, "getItemCount");
+            return moviesList.size();
         }
     }
 }
