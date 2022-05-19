44e413440ad654e98ed897a2f161c305995074a6
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2022-05-19 13:12:16 +0530

    bottom navigation view

---
+++ app/src/main/java/com/kriti/android1_30/BottomNavDemo.java
@@ -0,0 +1,14 @@
+package com.kriti.android1_30;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BottomNavDemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_nav_demo);
+    }
+}
