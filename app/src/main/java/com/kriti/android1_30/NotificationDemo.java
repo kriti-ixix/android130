c2dc862867c6b5d6c39eea81c9944c570dadc5cc
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2022-05-21 13:29:04 +0530

    notifications

---
+++ app/src/main/java/com/kriti/android1_30/NotificationDemo.java
@@ -0,0 +1,60 @@
+package com.kriti.android1_30;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class NotificationDemo extends AppCompatActivity {
+
+    Button button;
+    final String CHANNEL_ID = "1";
+    final int notificationId = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_demo);
+
+        button = findViewById(R.id.notificationButton);
+        createNotificationChannel();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(NotificationDemo.this, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.male_student)
+                        .setContentTitle("Title")
+                        .setContentText("Notification message")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(NotificationDemo.this);
+                notificationManager.notify(notificationId, builder.build());
+            }
+        });
+    }
+
+    private void createNotificationChannel()
+    {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            CharSequence name = "Channel Name";
+            String description = "Channel Description: This is my notification channel";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+}
